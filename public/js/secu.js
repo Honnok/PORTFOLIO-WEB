6646a3fbacf4b68e67bbcb04270bf28b61292803
Honnok <Hujiklo.bn@gmail.com>
2025-02-03 16:43:18 +0100

    V1.5.2

---
+++ public/js/secu.js
@@ -0,0 +1,6 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', event => {
+    if (event.ctrlKey && (event.key === 'u' || event.key === 'U')) {
+        event.preventDefault();
+    }
+});
