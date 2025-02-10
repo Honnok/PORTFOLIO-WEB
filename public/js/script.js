07bafd8d096217d55c1b88cb4228fe3ac8bffcc3
Honnok <Hujiklo.bn@gmail.com>
2025-02-10 11:39:01 +0100

    1.5.5
    Mise à jour Colone animation Menu principale

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,23 @@ const header = document.getElementById("header");
                 header.classList.remove("scrolled");
             }
         });
+
+document.addEventListener("DOMContentLoaded", function() {
+// Animation pour la barre Web Design
+gsap.fromTo(".barfillhtml", 
+    { height: "0%" }, 
+    { height: "100%", duration: 2, ease: "power2.out" }
+);
+        
+ // Animation pour la barre Python
+ gsap.fromTo(".barfillcss", 
+    { height: "0%" }, 
+    { height: "100%", duration: 2, ease: "power2.out", delay: 0.5 }
+);
+        
+// Animation pour la barre C#
+gsap.fromTo(".barfillc", 
+    { height: "0%" }, 
+    { height: "100%", duration: 2, ease: "power2.out", delay: 1 }
+);
+});
